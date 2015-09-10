263e9ebe0737eb78243c88caa6a85707e6044a23
Ryan Bent <ryanbent01@gmail.com>
2015-09-10 16:58:36 -0500

    add video model

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,5 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+Video.create(title: 'Monk', description: 'This is description for the myflix clone.', small_cover_url:'')
