5b4d9a47a9ff291b2f5eeef53e6a627af08b388c
Ryan Bent <ryanbent01@gmail.com>
2015-09-10 17:50:40 -0500

    Add seed data and create video model

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,4 +6,10 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
-Video.create(title: 'Monk', description: 'This is description for the myflix clone.', small_cover_url:'')
+
+
+Video.create(title: 'Fellowship Of The Ring', description: 'Story about a hobbit or something like that.',
+  small_cover_url: '/tmp/fellowship.jpg', large_cover_url: '/tmp/large.jpg')
+
+Video.create(title: 'Family Guy', description: 'Seth McFarlene animation show about Peter Griffin.',
+  small_cover_url: '/tmp/family_guy.jpg', large_cover_url: '/tmp/large.jpg')
