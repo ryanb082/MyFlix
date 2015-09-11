fddc9358db5189d86eec4151266b346a6eb1544e
Ryan Bent <ryanbent01@gmail.com>
2015-09-11 17:39:55 -0500

    get search function to pass rspec test

--- app/models/video.rb
+++ app/models/video.rb
@@ -2,5 +2,9 @@ class Video < ActiveRecord::Base
   belongs_to :category
 
   validates_presence_of :title, :description
+
+  def self.search_by_title(search_term)
+    where("title LIKE ?", "#{search_term}")
+  end
   
 end
--- spec/models/video_spec.rb
+++ spec/models/video_spec.rb
@@ -4,4 +4,24 @@ describe Video do
   it { should belong_to(:category)} 
   it { should validate_presence_of(:title)}
   it { should validate_presence_of(:description)}
+
+  describe "search_by_title" do
+    it "returns an empty array if there is no match" do
+      futurama = Video.create(title: 'Futurama', 
+      description: 'show in the future')
+      back_to_the_future = Video.create(title: 'Back to The Future', 
+      description: 'show in the past')
+      expect(Video.search_by_title(title: 'hello')).to eq([])
+    end
+    it "returns a array of one video for an exact match" do
+      futurama = Video.create(title: 'Futurama', 
+      description: 'show in the future')
+      back_to_the_future = Video.create(title: 'Back to The Future', 
+      description: 'show in the past')
+      expect(Video.search_by_title(title: 'Back to The Future')).to eq([back_to_the_future])
+    end
+
+    it "returns a array of one video for a partial match"
+    it "returns a array of all matches ordered by created_at"
+  end
 end
