88def597a79b9318f196d689dc45af264e07444f
Ryan Bent <ryanbent01@gmail.com>
2015-09-11 18:10:23 -0500

    create search function in model

--- spec/models/video_spec.rb
+++ spec/models/video_spec.rb
@@ -18,10 +18,32 @@ describe Video do
       description: 'show in the future')
       back_to_the_future = Video.create(title: 'Back to The Future', 
       description: 'show in the past')
-      expect(Video.search_by_title('Back to The Future')).to eq([back_to_the_future])
+      expect(Video.search_by_title('Futurama')).to eq([futurama])
     end
 
-    it "returns a array of one video for a partial match"
-    it "returns a array of all matches ordered by created_at"
+    it "returns a array of one video for a partial match" do
+      futurama = Video.create(title: 'Futurama', 
+      description: 'show in the future')
+      back_to_the_future = Video.create(title: 'Back to The Future', 
+      description: 'show in the past')
+      expect(Video.search_by_title('urama')).to eq([futurama])
+    end
+
+    it "returns a array of all matches ordered by created_at" do
+      futurama = Video.create(title: 'Futurama', 
+      description: 'show in the future', created_at: 1.day.ago)
+      back_to_the_future = Video.create(title: 'Back to The Future', 
+      description: 'show in the past')
+      expect(Video.search_by_title('Futur')).to eq([back_to_the_future,
+      futurama])
+    end
+
+    it "return an empty array for a search with an empty string" do
+      futurama = Video.create(title: 'Futurama', 
+      description: 'show in the future', created_at: 1.day.ago)
+      back_to_the_future = Video.create(title: 'Back to The Future', 
+      description: 'show in the past')
+      expect(Video.search_by_title("")).to eq([])
+    end
   end
 end
