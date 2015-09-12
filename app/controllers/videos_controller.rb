911b2cdd42ada56114102041ef53968a5f546dac
Ryan Bent <ryanbent01@gmail.com>
2015-09-11 20:01:30 -0500

    fix uninitialized constant error between videos controller::categoreis

--- app/controllers/videos_controller.rb
+++ app/controllers/videos_controller.rb
@@ -1,7 +1,7 @@
 class VideosController < ApplicationController
 
   def index
-    @videos = Video.all
+    @categories = Categories.all
   end
 
   def show
--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Myflix::Application.routes.draw do
   get '/home', to: 'videos#index'
   resources :videos, only: [:show, :index] do
     collection do
+      post :search, to: 'videos#search'
       get :search, to: 'videos#search'
     end
   end
