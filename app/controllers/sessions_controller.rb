f183ac5b51a27496791097ec1c6624317365a7db
Ryan Bent <ryanbent01@gmail.com>
2015-09-14 19:22:37 -0500

    Change sign in notice in UI

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,17 +7,19 @@ class SessionsController < ApplicationController
   def create
     user = User.find_by(email: params[:email])
     if user && user.authenticate(params[:password])
-      session[:user_id] = user.id
-      redirect_to home_path, notice: 'You are signed in.'
+      session[:user_id] = user.id 
+      flash[:success] = 'You are signed in.'
+      redirect_to home_path
     else
-      flash[:errro] = 'Invalid email or password.'
+      flash[:error] = 'Invalid email or password.'
       redirect_to sign_in_path
     end
   end
 
   def destroy
     session[:user_id] = nil
-    redirect_to root_path, notice: 'You have signed out.'
+    flash[:notice] = 'You have signed out.'
+    redirect_to root_path
   end
    
 end
