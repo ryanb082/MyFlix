9deed2649049dbe8a759f54d94bc63f61db9571b
Ryan Bent <ryanbent01@gmail.com>
2015-09-20 15:28:18 -0500

    add delete function to my queue

--- config/routes.rb
+++ config/routes.rb
@@ -13,7 +13,7 @@ Myflix::Application.routes.draw do
     resources :reviews, only: [:create]
   end
   resources :categories, only: [:show]
-  resources :queue_items, only: [:create]
+  resources :queue_items, only: [:create, :destroy]
 
   get '/my_queue', to: 'queue_items#index'
 
--- spec/controllers/queue_items_controller_spec.rb
+++ spec/controllers/queue_items_controller_spec.rb
@@ -54,18 +54,47 @@ describe QueueItemsController do
 #      south_park_queue_item = QueueItem.where(video_id: south_park.id, user_id: alice.id).first
 #      expect(south_park_queue_item.position).to eq(2) 
 #    end
-    it "does not add the video the queue if the video is already in the queue" do
-      alice = Fabricate(:user)
-      session[:user_id] = alice.id
-      monk = Fabricate(:video)
-      Fabricate(:queue_item, video: monk, user: alice)
-      south_park = Fabricate(:video)
-      post :create, video_id: monk.id
-      expect(alice.queue_items.count).to eq(1) 
-    end
+    #it "does not add the video the queue if the video is already in the queue" do
+#      alice = Fabricate(:user)
+#      session[:user_id] = alice.id
+#      monk = Fabricate(:video)
+#      Fabricate(:queue_item, video: monk, user: alice)
+#      south_park = Fabricate(:video)
+#      post :create, video_id: monk.id
+#      expect(alice.queue_items.count).to eq(1) 
+#    end
     it "redirects to the sign in page for unauthenticated users" do 
       post :create, video_id: 3
-      expect(response).to redirect_to to sign_in_path 
+      expect(response).to redirect_to sign_in_path 
     end
+  end
+
+  describe "DELETE destroy" do
+    it 'redirects to the my queue page' do
+      session[:user_id] = Fabricate(:user).id
+      queue_item = Fabricate(:queue_item)
+      delete :destroy, id: queue_item.id
+      expect(response).to redirect_to my_queue_path
+    end
+    it 'deletes the queue item' do
+      alice = Fabricate(:user)
+      session[:user_id] = alice.id
+      queue_item = Fabricate(:queue_item, user: alice)
+      delete :destroy, id: queue_item.id
+      expect(QueueItem.count).to eq(0)
+    end
+    it 'does not delete the queue item if the current user does not own the queue item' do
+      alice = Fabricate(:user)
+      bob = Fabricate(:user)
+      session[:user_id] = alice.id
+      queue_item = Fabricate(:queue_item, user: bob)
+      delete :destroy, id: queue_item.id
+      expect(QueueItem.count).to eq(1)
+    end
+    it 'redirects to the sign path for unauthenticated users' do
+      delete :destroy, id: 3
+      expect(response).to redirect_to sign_in_path
+    end
+
   end
 end
